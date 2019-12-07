b8befc09341ad42c54963b3c35f2112d89658c6d
michaeltconnelly <mconnelly@rsmas.miami.edu>
2019-12-07 14:37:39 -0500

    create all fasta file first

--- bash/cnid_makeblastdb.sh
+++ bash/cnid_makeblastdb.sh
@@ -16,18 +16,20 @@ prodir="/scratch/projects/transcriptomics/mikeconnelly/projects/coral_immunogeno
 
 #9 RefSeq genome assemblies
 refs="GCF_000222465.1_Adig_1.1 GCF_004143615.1_amil_sf_1.1 GCF_002571385.1_Stylophora_pistillata_v1 GCF_003704095.1_ASM370409v1 GCF_002042975.1_ofav_dov_v1 GCF_004324835.1_DenGig_1.0 GCF_001417965.1_Aiptasia_genome_1.1 GCF_000209225.1_ASM20922v1"
+touch ${prodir}/data/proteomes/db_protein_all.faa
 for ref in $refs
 do
   gunzip ${prodir}/data/proteomes/${ref}_protein.faa.gz
+  cat ${prodir}/data/proteomes/${ref}_protein.faa.gz >> ${prodir}/data/proteomes/db_protein_all.faa
 done
 
 #Protein blast databases from protein fasta files
 /share/apps/blast+/2.2.29/bin/makeblastdb \
 -dbtype prot \
 -parse_seqids \
--in ${prodir}/data/proteomes/*_protein.faa \
+-in ${prodir}/data/proteomes/db_protein_all.faa \
 -input_type 'fasta' \
--out ${prodir}/data/cnid_RefSeq_protblastdb \
+-out ${prodir}/data/blastdb_protein_all \
 -max_file_sz "4GB"
 
 for ref in $refs
