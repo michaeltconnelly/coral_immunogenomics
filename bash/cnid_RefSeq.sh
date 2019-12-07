c106bc91fdeb79f132308425ae3d0ee3a1511828
michaeltconnelly <mconnelly@rsmas.miami.edu>
2019-12-06 19:16:57 -0500

    proteomes directory

--- bash/cnid_RefSeq.sh
+++ bash/cnid_RefSeq.sh
@@ -13,43 +13,11 @@
 
 prodir="/scratch/projects/transcriptomics/mikeconnelly/projects/coral_immunogenomics"
 refseq="rsync://ftp.ncbi.nlm.nih.gov/genomes/all/GCF"
-
-#A. digitifera
+#A. digitifera, A. millepora, S. pistillata, P. damicornis, O. faveolata, D. gigantea, E. pallida, N. vectensis
+targets="000/222/465/GCF_000222465.1_Adig_1.1 004/143/615/GCF_004143615.1_amil_sf_1.1 002/571/385/GCF_002571385.1_Stylophora_pistillata_v1 003/704/095/GCF_003704095.1_ASM370409v1 002/042/975/GCF_002042975.1_ofav_dov_v1 004/324/835/GCF_004324835.1_DenGig_1.0 001/417/965/GCF_001417965.1_Aiptasia_genome_1.1 000/209/225/GCF_000209225.1_ASM20922v1"
+for target in $targets
+do
 rsync --copy-links --recursive --times --verbose \
-${refseq}/000/222/465/GCF_000222465.1_Adig_1.1 \
-${prodir}/data/genomes
-
-#A. millepora
-rsync --copy-links --recursive --times --verbose \
-${refseq}/004/143/615/GCF_004143615.1_amil_sf_1.1 \
-${prodir}/data/genomes
-
-#S. pistillata
-rsync --copy-links --recursive --times --verbose \
-${refseq}/002/571/385/GCF_002571385.1_Stylophora_pistillata_v1 \
-${prodir}/data/genomes
-
-#P. damicornis
-rsync --copy-links --recursive --times --verbose \
-${refseq}/003/704/095/GCF_003704095.1_ASM370409v1 \
-${prodir}/data/genomes
-
-#O. faveolata
-rsync --copy-links --recursive --times --verbose \
-${refseq}/002/042/975/GCF_002042975.1_ofav_dov_v1 \
-${prodir}/data/genomes
-
-#D. gigantea
-rsync --copy-links --recursive --times --verbose \
-${refseq}/004/324/835/GCF_004324835.1_DenGig_1.0 \
-${prodir}/data/genomes
-
-#E. pallida
-rsync --copy-links --recursive --times --verbose \
-${refseq}/001/417/965/GCF_001417965.1_Aiptasia_genome_1.1 \
-${prodir}/data/genomes
-
-#N. vectensis
-rsync --copy-links --recursive --times --verbose \
-${refseq}/000/209/225/GCF_000209225.1_ASM20922v1 \
-${prodir}/data/genomes
+${refseq}/${target}/*protein* \
+${prodir}/data/proteomes
+done
