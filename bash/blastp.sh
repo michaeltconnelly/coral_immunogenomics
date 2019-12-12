e93c8a1ea47bd10ab8b81246cd55f2e2737e1a5f
michaeltconnelly <mconnelly@rsmas.miami.edu>
2019-12-12 18:51:16 -0500

    moving towards blastp searches

---
+++ bash/blastp.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#purpose: Conduct blastp searched against NCBI RefSeq cnidarian genome assemblies
+
+#BSUB -J cnid_blastp
+#BSUB -q general
+#BSUB -P transcriptomics
+#BSUB -o blastp%J.out
+#BSUB -e blastp%J.err
+#BSUB -n 8
+#BSUB -u m.connelly1@umiami.edu
+
+module load blast/2.2.29+
+prodir="/scratch/projects/transcriptomics/mikeconnelly/projects/coral_immunogenomics"
+
+/share/apps/blast+/2.2.29/bin/blastp \
+-db \
+-query \
+-out
